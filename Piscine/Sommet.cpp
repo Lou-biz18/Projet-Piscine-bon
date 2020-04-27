5075cfdd9270d8580414f584ce24a7d7bdc20fa2
Lou-biz18 <bizet773@gmail.com>
2020-04-27 17:29:46 +0200

    changement dans sommet
    ajout d'une methode

--- Piscine/Sommet.cpp
+++ Piscine/Sommet.cpp
@@ -10,3 +10,10 @@ Sommet::~Sommet()
 {
     //dtor
 }
+void Sommet::afficher() const///afficher les données d'un bloc pour debug
+{
+    std::cout<<m_idSommet<<std::endl
+             <<m_nom<<std::endl
+             << m_coords.first << ", " << m_coords.second<<std::endl;
+
+}
