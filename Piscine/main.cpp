de7e6cd9d121a302477ca12a78956841f5849902
gaignardcarla <61588690+gaignardcarla@users.noreply.github.com>
2020-04-27 21:49:18 +0200

    vc
    v

--- Piscine/main.cpp
+++ Piscine/main.cpp
@@ -2,6 +2,12 @@
 
 int main()
 {
+<<<<<<< Updated upstream
     std::cout << "Hello world!" << std::endl;
     return 0;
+=======
+    cout << "Hello world!" << endl;
+
+    string =  "d" ;
+>>>>>>> Stashed changes
 }
