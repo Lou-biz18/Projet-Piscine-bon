86421ae1e5f52bdea623d4b9da4ca15e626decfc
Lou-biz18 <bizet773@gmail.com>
2020-04-28 11:35:58 +0200

    bon projet
    rfges

--- Piscine/main.cpp
+++ Piscine/main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
+#include "graphe.h"
+#include "Sommet.h"
+#include "Arete.h"
 
-using namespace std;
-
+void setup(Graphe&graphe);
 int main()
 {
-    cout << "Hello world!" << endl;
+    Graphe graphe;
+    setup(graphe);/////
     return 0;
+}
+void setup(Graphe&graphe)//muscle
+{
+    std::string monFichier="test.txt";
+    graphe.chargeGraphe(monFichier);
+    graphe.afficher();
+    std::cout << "Hello world!" << std::endl;
 }
